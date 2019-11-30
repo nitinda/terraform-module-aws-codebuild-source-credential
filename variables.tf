1710b81ae15dca6914ac5189df37fb4edb8b157d
nitin-das <nitin.das@thomson.co.uk>
2019-11-30 11:10:00 +0000

    Added new code

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "auth_type" {
+  description = "The type of authentication used to connect"
+}
+
+variable "server_type" {
+  description = "The source provider used for this project."
+}
+
+variable "token" {
+  description = "This is the personal access token"
+}
